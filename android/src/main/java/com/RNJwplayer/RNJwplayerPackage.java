ed7ea3bc216b5b84d6ff85857106ed7908ed4b3b
Faustino Kialungila <faustino.kialungila@gmail.com>
2019-09-08 17:11:48 +0200

    revert android stuff

--- android/src/main/java/com/RNJwplayer/RNJwplayerPackage.java
+++ android/src/main/java/com/RNJwplayer/RNJwplayerPackage.java
@@ -1,6 +1,3 @@
-
-package com.reactlibrary;
-
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -10,19 +7,15 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
+
 public class RNJwplayerPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNJwplayerModule(reactContext));
-    }
-
-    // Deprecated from RN 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
+        return Arrays.<NativeModule>asList(new RNJwplayerModule(reactContext));
     }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
+        return Collections.emptyList();
     }
 }
